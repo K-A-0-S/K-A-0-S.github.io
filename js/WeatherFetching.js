c84e0d0ab743b2fe80e106a0a94b4c04bf246bbd
K-A-0-S <kaoskwads@gmail.com>
2024-06-19 23:31:46 -0400

    Update WeatherFetching.js

--- js/WeatherFetching.js
+++ js/WeatherFetching.js
@@ -218,7 +218,7 @@ function fetchRadarImages(){
     "menu": false,
     "shortFusedOnly": false,
     "opacity": {
-      "alerts": 0.4,
+      "alerts": 0.0,
       "local": 0.0,
       "localStations": 0.0,
       "national": 0.9
@@ -258,7 +258,7 @@ function fetchRadarImages(){
       "menu": false,
       "shortFusedOnly": false,
       "opacity": {
-        "alerts": 0.4,
+        "alerts": 0.0,
         "local": 0.0,
         "localStations": 0.0,
         "national": 0.9
