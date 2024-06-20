c28bd89a04a7496957b5c13adab4eada804a7a90
K-A-0-S <kaoskwads@gmail.com>
2024-06-19 23:36:40 -0400

    Update Radar.js

--- js/Radar.js
+++ js/Radar.js
@@ -268,7 +268,7 @@ function fetchRadarImages(){
       "menu": false,
       "shortFusedOnly": false,
       "opacity": {
-        "alerts": 0.1,
+        "alerts": 0.0,
         "local": 0.0,
         "localStations": 0.0,
         "national": 0.6
