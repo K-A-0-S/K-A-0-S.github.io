64aa176b617b8646ef496aca63bf34ab3bba8ee7
K-A-0-S <kaoskwads@gmail.com>
2024-07-31 07:59:59 -0400

    Update MainScript.js

--- js/MainScript.js
+++ js/MainScript.js
@@ -96,7 +96,7 @@ function setInformation(){
   setCurrentConditions();
   setTimelineEvents();
   hideSettings();
-  setTimeout(startAnimation, 5000);
+  setTimeout(startAnimation, 9000);
 }
 
 function setMainBackground(){
