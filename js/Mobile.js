fa839ed773c8eb9fc82d80d6df05057ffc17fab9
K-A-0-S <kaoskwads@gmail.com>
2024-08-01 14:13:34 -0400

    Update Mobile.js

--- js/Mobile.js
+++ js/Mobile.js
@@ -276,7 +276,7 @@ function fetchRadarImages(){
   startRadar();
   //setTimeout(startRadar, 8000);
   //startZoomedRadar();
-  setTimeout(startZoomedRadar, 5000);
+  setTimeout(startZoomedRadar, 9000);
 
 
 }
